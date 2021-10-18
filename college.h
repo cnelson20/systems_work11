8d575de350b31e5bee96f107b37724d374f4018f
Cameron Nelson <72891927+cnelson20@users.noreply.github.com>
2021-10-18 11:45:50 -0400

    Update college.h

--- college.h
+++ college.h
@@ -1,1 +1,16 @@
+struct college; 
 
+struct college *make_college();
+void print_college(struct college *uni);
+void print_list(struct college *c); 
+struct college *insert_front(struct college *head, char *n, char *place,int body, int div);
+struct college *free_list(struct college *c);
+
+
+struct college {
+  char *name;
+  char *location;
+  int ncaa_div;
+  int student_population;
+  struct college *next;
+};
